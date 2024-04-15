d45224f7cce41c111bda1a6d2357c2672251b64c
Ofek <ofek7010@gmail.com>
2024-04-15 21:03:22 +0300

    allowing all hosts

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-%^fcb4vemu#jsw+v+tv!!f%*)5l7+!e-4%!hsf#s=4ef6qn@)$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
