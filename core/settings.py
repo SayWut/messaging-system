040e5905629913dcc37f1b43b872ab09b8ffac73
Ofek <ofek7010@gmail.com>
2024-04-15 20:29:43 +0300

    support Render
    - changed from default sqlite to POSTGRESQL on render

--- core/settings.py
+++ core/settings.py
@@ -14,6 +14,7 @@ from datetime import timedelta
 from pathlib import Path
 
 from django.conf import settings
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,15 +75,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+# Replace the SQLite DATABASES configuration with PostgreSQL:
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgres://messaging_system_qvu0_user:wgJ2eCt126NdtDU0kGfvubp6iJLVx09g@dpg-coem5n0l5elc738bt1qg-a/messaging_system_qvu0',
+        conn_max_age=600
+    )
 }
 
 
