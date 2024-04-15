299b7aa099ed3e256d857bf81447c9b6feba2e10
Ofek <ofek7010@gmail.com>
2024-04-15 18:37:33 +0300

    added build file

---
+++ build.sh
@@ -0,0 +1,13 @@
+# Exit on error
+set -o errexit
+
+# Modify this line as needed for your package manager (pip, poetry, etc.)
+pip install -r requirements.txt
+
+# Apply any outstanding database migrations
+python manage.py migrate --run-syncdb
+
+if [[ $CREATE_SUPERUSER ]];
+then
+  python manage.py createsuperuser --no-input
+fi
